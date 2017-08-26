6fc0f1b95ac727ef36576eead66d2311d532fe80
Marco Eilers <marco.eilers@inf.ethz.ch>
2017-08-26 16:04:47 +0200

    Tried adding current member to returned error

--- src/main/scala/rules/SymbolicExecutionRules.scala
+++ src/main/scala/rules/SymbolicExecutionRules.scala
@@ -9,6 +9,7 @@ package viper.silicon.rules
 import viper.silicon.{JSTreeRenderer, SymbExLogger}
 import viper.silicon.interfaces.Failure
 import viper.silicon.verifier.Verifier
+import viper.silver.ast.Member
 import viper.silver.verifier.{AbstractVerificationError, VerificationError}
 import viper.silver.verifier.errors.VerificationErrorWithCounterexample
 
@@ -22,7 +23,8 @@ trait SymbolicExecutionRules extends Immutable {
       val jsRenderer = new JSTreeRenderer()
       val symState = jsRenderer.render(SymbExLogger.memberList)
       SymbExLogger.printError(ve.toString, model)
-      Failure(VerificationErrorWithCounterexample(ve.asInstanceOf[AbstractVerificationError], model, symState))
+      val currentMember = if (SymbExLogger.memberList.length > 0 && SymbExLogger.memberList.last.main.isInstanceOf[Member]) SymbExLogger.memberList.last.main.value.asInstanceOf[Member].name else "UNKNOWN"
+      Failure(VerificationErrorWithCounterexample(ve.asInstanceOf[AbstractVerificationError], model, symState, currentMember))
     }else {
       Failure(ve)
     }
