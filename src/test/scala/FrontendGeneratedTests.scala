9e562c946ce930e844bed2b9c1bbdc783aec789c
Mauro Bringolf <maurob@student.ethz.ch>
2020-03-13 15:36:37 +0100

    Do not discover frontend tests

--- src/test/scala/FrontendGeneratedTests.scala
+++ src/test/scala/FrontendGeneratedTests.scala
@@ -1,5 +1,8 @@
 package viper.silicon.tests
 
+import org.scalatest.DoNotDiscover
+
+@DoNotDiscover
 class FrontendGeneratedTests extends PortableSiliconTests {
   override val testDirectories = Seq("src/test/resources/frontend-generated")
 }
